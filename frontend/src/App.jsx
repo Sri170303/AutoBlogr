eea5c00f254689a3ed0e749fbf407858fb3729e5
Sri Darsan Sah <sridarsansah5907@gmail.com>
2025-06-23 03:16:10 +0530

    mounted navbar on the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <div>
+        <NavBar />
         <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/blog' element={<Blog />}/>
