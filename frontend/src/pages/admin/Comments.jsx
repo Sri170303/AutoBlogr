42f4065841e211c618a42437598a54a2aad4d46a
Sri Darsan Sah <sridarsansah5907@gmail.com>
2025-06-27 22:34:52 +0530

    remove typo in background addition

--- frontend/src/pages/admin/Comments.jsx
+++ frontend/src/pages/admin/Comments.jsx
@@ -15,7 +15,7 @@ const Comments = () => {
   }, [])
 
   return (
-    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-bule-50/50'>
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50'>
       <div className='flex justify-between items-center max-w-3xl'>
         <h1>Comments</h1>
         <div className='flex gap-4'>
