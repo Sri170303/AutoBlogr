a9e309aac7fdab6e40e0f77c58ef8ffa441e4e43
Sri Darsan Sah <sridarsansah5907@gmail.com>
2025-07-04 12:01:13 +0530

    complete the API to add blogs using imageKit

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import imagekit from '../configs/imagekit.js';
+import Blog from '../models/blog.js';
 
 export const addBlog = async (req, res) =>{
     try {
@@ -8,9 +11,35 @@ export const addBlog = async (req, res) =>{
 
         if(!title || !description || !category || !imageFile)
         {
-            return res.json({success: false, message: "Missing required fields"})
+            return res.json({success: false, message: "Missing required fields"});
         }
+
+        const fileBuffer = fs.readFileSync(imageFile.path);
+
+        // upload image to ImageKit
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/blogs'
+        })
+
+        // optimise image through imageKit and get URL of transformed image
+        const optimizedImageURL = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {quality: 'auto'},
+                {format: 'webp'},
+                {width: '1280'}
+            ]
+        })
+
+        const image = optimizedImageURL;
+
+        await Blog.create({title, subTitle, description, category, image, isPublished});
+
+        res.json({success: true, message: "Blog added successfull"});
+
     } catch (error) {
-        
+        res.json({success: false, message: error.message});
     }
 }
