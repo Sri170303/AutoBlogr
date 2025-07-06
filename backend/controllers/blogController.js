da1a2395c90af96cd35520823cda0dfa998a18d8
Saket Kumar <saketkumar4634@gmail.com>
2025-07-06 13:42:35 +0530

    create API for blog comments

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import imagekit from '../configs/imagekit.js';
 import Blog from '../models/blog.js';
+import Comment from '../models/Comment.js';
 
 export const addBlog = async (req, res) =>{
     try {
@@ -89,6 +90,27 @@ export const togglePublish = async(req, res) =>
         blog.isPublished = !blog.isPublished ;
         await blog.save() ;
         res.json({success:true, message: "Blog status updated"})
+    } catch (error) {
+        res.json({success:false, message:error.message})
+    }
+}
+
+export const addComment = async (req, res) => {
+    try {
+        const {blog, name, content} = req.body;
+        await Comment.create({blog, name, content});
+        res.json({success:false, message:'Comment added for review'})
+    } catch (error) {
+         res.json({success:false, message:error.message})
+    }
+}
+
+export const getBlogComments = async (req, res) =>
+{
+    try {
+        const {blogId} = req.body;
+        const comments = await Comment.find({blog: blogId, IsApproved: true}).sort({createdAt: -1});
+        res.json({success:false, comments})
     } catch (error) {
         res.json({success:false, message:error.message})
     }
